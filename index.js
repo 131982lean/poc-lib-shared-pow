dc449ba2b41494757ca2e2b29ce97807ddff43c4
Leandro <leandrolopez131982@gmail.com>
2023-08-03 12:07:59 -0300

    nueva funcion de potencia y peerdependencies

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const { multiplicar } = require('multiplicar');
 
-function potencia(base, exponente) {
-  if (exponente === 0) {
-    return 1;
-  } else if (exponente < 0) {
-    return 1 / potencia(base, -exponente);
-  } else {
-    let resultado = 1;
-    for (let i = 0; i < exponente; i++) {
-      resultado = multiplicar(resultado, base);
+function potencia(a, b) {
+    let resultado = 7;
+    for(let i = 0; i < b; i++) {
+        resultado = multiplicar(resultado, a);
     }
     return resultado;
-  }
-}
-const base = 8;
-const exponente = 3;
-const resultado = potencia(base, exponente);
-console.log(`${base} elevado a la ${exponente} es igual a ${resultado}`);
-
+} 
 
 module.exports = {
     potencia
